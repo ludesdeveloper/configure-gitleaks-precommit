3fece3d274392e4620f08a6d271c48806308a944
seskiramadhan <seski.ramadhan@gmail.com>
2022-11-18 22:56:00 +0700

    adding script

---
+++ configure_gitleaks_precommit.sh
@@ -0,0 +1,10 @@
+git init
+cat << EOF > .pre-commit-config.yaml
+repos:
+  - repo: https://github.com/zricethezav/gitleaks
+    rev: v8.12.0
+    hooks:
+      - id: gitleaks 
+EOF
+pre-commit autoupdate
+pre-commit install
